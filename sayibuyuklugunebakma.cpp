241748612c44f385702bcccc03e83288365e92b1
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-09 17:56:43 +0300

    Create sayibuyuklugunebakma.cpp

---
+++ sayibuyuklugunebakma.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+void main()
+{
+	int sayi1, sayi2;
+	cout << "birinci sayi...";
+	cin >> sayi1;
+	cout << "ikinci sayi...";
+	cin >> sayi2;
+	if (sayi1 > sayi2)
+		cout << "sayi 1 daha buyuk\n";
+	else if (sayi1 < sayi2)
+		cout << "sayi 2 daha buyuk\n";
+	else if (sayi1 = sayi2)
+		cout <<endl <<"birbirine esittir.\n";
+	 
+
+    
+	
+	system("PAUSE");
+}
