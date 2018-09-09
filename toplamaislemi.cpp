fc2098991fc8a5f9170cd217f225351ecb8de4b0
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-09 17:58:31 +0300

    Create toplamaislemi.cpp

---
+++ toplamaislemi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int sayi1, sayi2;
+	cout << "lutfen birinci sayiyi giriniz:";
+	cin >> sayi1;
+	cout << "lutfen ikinci sayiyi giriniz:";
+	cin >> sayi2;
+	int sonuc = sayi1 + sayi2;
+    cout << sonuc <<endl ;
+	
+		 
+
+    
+	
+	system("PAUSE");
+}
