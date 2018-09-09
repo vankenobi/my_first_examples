6485e9cb0cc43d3d9c140006f73be80492e61810
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-09 17:57:41 +0300

    Create switchcase.cpp

---
+++ switchcase.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int sayi = 3;
+	
+	switch (sayi)
+	{
+	case 1:
+		cout << "merhaba 1. program calisiyo...\n";
+		break;
+	case 2:
+		cout << "merhaba 2. program calisiyo...\n";
+		break;
+	case 3:
+		cout << "merhaba 3. program calisiyo...\n";
+		break;
+	default:
+		cout << "merhaba hicbir program calismiyor...\n";
+
+
+
+	}
+
+
+
+	system("PAUSE");
+	
+}
