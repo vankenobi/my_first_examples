257c847f08a69ca8e377b14d5afc74e3bad49178
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-09 18:00:54 +0300

    Create Veya operatörü.cpp

---
+++ Veya operatörü.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "birinci sayi: ";
+	cin >> a;
+	cout << "ikinci sayi: ";
+	cin >> b;
+	bool c = a <= 20 || b < 50;
+	cout << "c'nin degeri: " << c<<endl;
+
+
+
+	
+
+	system("PAUSE");
+	
+}
