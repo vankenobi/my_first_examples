d87aca9ec2b9d0fc172f05a8e9e08398a25e72ec
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-10 23:19:24 +0300

    Create iki nokta arası uzaklık.cpp

---
+++ iki nokta arası uzaklık.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double sonuc;
+	int x1, y1, x2, y2;
+	cout << "Lutfen birinci noktanin x degerini giriniz: ";
+	cin >> x1;
+	cout << "Lutfen birinci noktanin y degerini giriniz: ";
+	cin >> y1;
+	cout << "Lutfen ikinci noktanin x degerini giriniz: ";
+	cin >> x2;
+	cout << "Lutfen ikinci sayinin y degerini giriniz: ";
+	cin >> y2;
+	sonuc = sqrt(pow((x2 - x1),(2)) + pow((y2 - y1),(2)));
+	
+	cout << sonuc<<endl;
+	
+
+	system("PAUSE");
+	
+}
