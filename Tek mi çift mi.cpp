1ce00f648b96e6659427108965a6866d43151e8b
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-10 22:06:11 +0300

    Create Tek mi çift mi.cpp

---
+++ Tek mi çift mi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int mod;
+	int sayi;
+	cout << "Lutfen bir sayi giriniz: ";
+	cin >> sayi;
+	mod = sayi % 2;
+	cout << "Sonuc:";
+	(mod == 0) ? cout << "sayi cift\n" : cout << "Sayi tek\n";
+	
+
+	system("PAUSE");
+	
+}
