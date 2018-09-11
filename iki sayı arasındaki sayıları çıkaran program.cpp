759eb6dc6ea446cae571b093518f3d835552eddf
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-11 23:21:34 +0300

    Create iki sayı arasındaki sayıları çıkaran program.cpp

---
+++ iki sayı arasındaki sayıları çıkaran program.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+
+   int sayi1, sayi2;
+   cout << "Lutfen kucuk sayiyi  giriniz: ";
+   cin >> sayi1;
+   cout << "Lutfen buyuk sayiyi giriniz: ";
+   cin >> sayi2;
+
+   for (sayi1; sayi1 <= sayi2; sayi1++)
+   {
+	   cout << sayi1<<",";
+   
+   }
+   
+   
+	system("PAUSE");
+	
+}
