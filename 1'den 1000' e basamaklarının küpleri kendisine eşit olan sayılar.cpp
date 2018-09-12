eb1a5f5278345638047e3062ccbfefcab0e65e0a
vankenobi <43112588+vankenobi@users.noreply.github.com>
2018-09-12 21:23:13 +0300

    Create 1'den 1000' e basamaklarının küpleri kendisine eşit olan sayılar.cpp

---
+++ 1'den 1000' e basamaklarının küpleri kendisine eşit olan sayılar.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 100; i < 1000; i++)
+	{
+	
+		int xb = i % 10;
+		int sonuc = i / 10;
+	    int xo = sonuc % 10;
+		int ssonuc = sonuc / 10;
+		if (pow(xb, 3) + pow(xo, 3) + pow(ssonuc, 3) == i)
+		{
+			cout << "Sonuc: ";
+			cout << i;
+		}
+
+	
+	
+	}
+
+	system("PAUSE");
+}
