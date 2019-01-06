ca3db8ac42582a673e1654cc3732154f5e42c9ad
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-01-06 15:25:07 +0300

    Create Fonksiyon#1.cpp

---
+++ Fonksiyon#1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+void selamla();
+int main()
+{
+	selamla();
+	selamla();
+	system("PAUSE");
+}
+void selamla(){
+	cout << "Merhaba"<<endl;
+	cout << "Nasilsin" << endl;
+	
+}
