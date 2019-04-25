5a382c8fff223031b21e94a99ad38533d741251d
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-04-25 23:39:51 +0300

    Create Class #1.cpp

---
+++ Class #1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+class dikdortgen
+{
+	 char ad[10];
+	public:
+		
+		//constructor// construcctor paramterli de olabilir parametresiz de.
+		dikdortgen(char p[])
+		{
+	    strcpy(ad,p);
+		cout<<"-----------------------------"<<endl;
+	    cout<<"paramtreli constructor"<<endl;
+		cout<<"parametrem:"<<p<<endl;
+		cout<<"-----------------------------"<<endl;
+		}
+		
+		//Destructor// 
+		//destructor parametresiz olur her zaman.
+		~dikdortgen()
+		{
+		cout<<ad<<" ";
+		cout<<"yok edildi."<<endl;
+		}
+		
+};
+int main()
+{
+    
+    {
+    	dikdortgen d1("d1");
+    	{
+    	dikdortgen d2("d2");
+		}
+	}
+	return 0;
+}
