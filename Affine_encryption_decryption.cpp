5beec360882fff023372899a0766e912cbc46f68
vankenobi <43112588+vankenobi@users.noreply.github.com>
2019-12-18 01:35:55 +0300

    Update Affine_encryption_decryption.cpp

--- Affine_encryption_decryption.cpp
+++ Affine_encryption_decryption.cpp
@@ -15,7 +15,7 @@ struct node
 	node *next;
 };
 
-node *add(node *r,char char_,int number)
+node *add(node *r,char char_,int number) 
 {
 	if(r == NULL)
 	{
@@ -39,9 +39,9 @@ node *add(node *r,char char_,int number)
 	}
 	return r;
 }
-node *preloading(node *r)
+node *preloading(node *r) // preloading fonksiyonu ile harfleri listeye aktarıyoruz ve liste başını döndürüyoruz.
 {
-	int counter = 0;
+	int counter = 0;	//sayıcı harflere indis değerlerinin atılması için gerekli
 	for(int i=97;i<=122;i++)
 	{
 		r = add(r,i,counter);
@@ -49,15 +49,15 @@ node *preloading(node *r)
 	}
 	return r;
 }
-int number_finding(node *r,string str)
-{
-	node *yrd;
+int number_finding(node *r,string str) // Numara bulma fonksiyonu gelen harfin indis değerini liste içerisinden bulup döndürür.
+{									  
+	node *yrd;	
 	yrd = r;
-	string s;
+	string s;	//Char tipindeki liste elemanını string veriye dönüştürürken kullanıyoruz.
 	while (yrd != NULL)
 	{
-		s = yrd->char_;
-		if (s == str)
+		s = yrd->char_; // char --> string
+		if (s == str)	
 		{
 			return yrd->number;
 			break;
@@ -68,14 +68,14 @@ int number_finding(node *r,string str)
 	delete yrd;
 	return 0;
 }
-string char_finding(node *r,int number)
+string char_finding(node *r,int number)	//gelen indis değerinin karşılığı olan karakteri bulup döndürür.
 {
 	string s;
 	while(r != NULL)
 	{
 		if(number  == r->number)
 		{
-			s = r->char_;
+			s = r->char_;	//char --> string
 			return s;
 			break;
 		}
@@ -84,14 +84,14 @@ string char_finding(node *r,int number)
 	return 0;
 }
 
-string encoding(node *r,string text,int a,int b)
+string encoding(node *r,string text,int a,int b)	//gelen texti y = ax+b formülüne göre şifreler
 {
-	string encode_text;
-	int len = text.length();
-	for(int i=0;i<len;i++)
+	string encode_text;		 // şifrelenmiş text
+	int len = text.length(); // gelen textin boyutunu len değişkenine atadım.
+	for(int i=0;i<len;i++)	 
 	{
-		int number = (a*number_finding(r,text.substr(i,1))+b) % 25;
-		encode_text += char_finding(r,number);
+		int number = (a*number_finding(r,text.substr(i,1))+b) % 25; //substr metodu string içerisinde hareket etmemizi sağlar.
+		encode_text += char_finding(r,number);	//şifrelenen her indis için liste içerisinde hangi harfe denk geldiğini bulur ve encode_text değişkenine aktarır.
 	}
 	return encode_text;
 }
@@ -100,28 +100,28 @@ string decoding(node *r,string str,int a,int b)
 {
 	int len = str.length();
 	string decode_text;
-	int c_inv;
+	int c_inv;	
 	int c;
 	for (int k=0;k<25;k++)
 	{
-		c = (a*k) % 25;
+		c = (a*k) % 25;			//decoding için bir sabit bulmamız gerekiyor.
 		if (c == 1)
 		{
-			c_inv = k; 
+			c_inv = k;			//bu sabit c_inv değişkenine aktarılıyor.
 		}
 	}
 
 	for(int i=0;i<len;i++)
 	{
-		int x = number_finding(r,str.substr(i,1));
-		int number = (c_inv*(x-b))	% 25;
-		decode_text += char_finding(r,number);
-	}
+		int x = number_finding(r,str.substr(i,1));	//gelen şifrelenmiş texti harflerinin numaralarını liste içerinden bulur ve x değişkenine atar.
+		int number = (c_inv*(x-b))	% 25;			// formül c_inc*(x-b) mod n
+		decode_text += char_finding(r,number);		//karakter bulma fonksiyonu ile girdisi gerçekleşen numara değerine karşılık gelen karakter döndürülür ve decode_text
+	}												//text değişkenine atılır.
 	return decode_text;
 
 }
 
-void display(node *r)
+void display(node *r)	//Display fonksiyonu ile alfabenin çıktısını alıyoruz.
 {
 	while(r != NULL)
 	{
@@ -130,12 +130,13 @@ void display(node *r)
 	}
 	system("Pause");
 }
+
 int main()
 {
 	node *root;
 	root = NULL;
 
-	string text = "mustafa";
+	string text = "asdfghjkls";	
 
 	string decode_text;
 	string encode_text;
