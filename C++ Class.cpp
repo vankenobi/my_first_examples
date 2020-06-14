7a20dbc9af6c29a2d12ba361245788d85744bfca
vankenobi <43112588+vankenobi@users.noreply.github.com>
2020-06-14 20:47:07 +0300

    Update and rename Class #1.cpp to C++ Class.cpp

--- C++ Class.cpp
+++ C++ Class.cpp
@@ -5,35 +5,35 @@ using namespace std;
 class dikdortgen
 {
 	 char ad[10];
-	public:
+		public:
 		
 		//constructor// construcctor paramterli de olabilir parametresiz de.
 		dikdortgen(char p[])
 		{
-	    strcpy(ad,p);
-		cout<<"-----------------------------"<<endl;
-	    cout<<"paramtreli constructor"<<endl;
-		cout<<"parametrem:"<<p<<endl;
-		cout<<"-----------------------------"<<endl;
+			strcpy(ad,p);
+			cout<<"-----------------------------"<<endl;
+			cout<<"paramtreli constructor"<<endl;
+			cout<<"parametrem:"<<p<<endl;
+			cout<<"-----------------------------"<<endl;
 		}
 		
-		//Destructor// 
-		//destructor parametresiz olur her zaman.
+		//Destructor
+		//destructor parametre almaz.
 		~dikdortgen()
 		{
-		cout<<ad<<" ";
-		cout<<"yok edildi."<<endl;
+			cout<<ad<<" ";
+			cout<<"yok edildi."<<endl;
 		}
 		
 };
+
 int main()
 {
-    
     {
     	dikdortgen d1("d1");
-    	{
+    {
     	dikdortgen d2("d2");
-		}
-	}
+    }
+}
 	return 0;
 }
